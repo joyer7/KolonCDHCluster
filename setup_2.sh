79d348043189ebf2081d68ea7093ec026c11c302
injoyer <injoyer@gmail.com>
2019-12-20 13:59:56 +0900

    Devide setup script

--- setup_2.sh
+++ setup_2.sh
@@ -91,23 +91,3 @@ done
 
 echo "-- Now CM is started and the next step is to automate using the CM API"
 
-# Cloudera Manager Install
-yum install -y epel-release
-yum install -y python-pip
-pip install --upgrade pip
-pip install cm_client
-sed -i "s/YourHostname/`hostname -f`/g" ~/KolonCDHCluster/$TEMPLATE
-sed -i "s/YourCDSWDomain/cdsw.$PUBLIC_IP.nip.io/g" ~/KolonCDHCluster/$TEMPLATE
-sed -i "s/YourPrivateIP/`hostname -I | tr -d '[:space:]'`/g" ~/KolonCDHCluster/$TEMPLATE
-sed -i "s#YourDockerDevice#$DOCKERDEVICE#g" ~/KolonCDHCluster/$TEMPLATE
-sed -i "s/YourHostname/`hostname -f`/g" ~/KolonCDHCluster/scripts/create_cluster.py
-
-# Create Cluster
-# Python Installation
-python ~/KolonCDHCluster/scripts/create_cluster.py $TEMPLATE
-
-# configure and start EFM and Minifi
-service efm start
-#service minifi start
-
-echo "-- At this point you can login into Cloudera Manager host on port 7180 and follow the deployment of the cluster"
